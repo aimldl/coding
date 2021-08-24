70ddbfaa454ab92fd072ee8dc614ecc330b34a70
aimldl <mrtonnet@gmail.com>
2021-08-24 14:09:43 +0900

    Create the_general_form_of_a_cpp_program-p_288.cpp

---
+++ c++/en/books/The_Complete_Reference_CPP_4th_Edition/archive_from_icloud_memo/the_general_form_of_a_cpp_program-p_288.cpp
@@ -0,0 +1,17 @@
+// The General Form of a C++ Program, p.288
+
+Although individual styles will differ, most C++ programs will have this general form:
+
+#includes
+base-class declarations
+derived class declarations
+nonmember function prototypes
+
+int main()
+{
+  // ...
+}
+
+nonmember function definitions
+
+In most large projects, all class declarations will be put into a header file and included with each module. But the general organization of a program remains the same.
